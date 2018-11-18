53510b9dad3d44544418744c255b7058c6065528
Sherlock <Sherlock@192.168.48.193>
2018-11-18 12:34:15 +0530

    Added some details

--- APproject/src/data/Store.java
+++ APproject/src/data/Store.java
@@ -2,14 +2,22 @@ package data;
 
 public class Store implements Superstore {
 	StoreAdministrator Admin;
-	private String LinkedWare_string;
+//	private String LinkedWare_string;
 	Warehouse LinkedWare_obj;
 	private final int MaxItems;
 	
-	String getware()
-	{
-		return this.LinkedWare_string;
+	//CONSTRUCTOR
+	Store(StoreAdministrator sa,Warehouse w,int max){
+		this.Admin=sa;
+		this.LinkedWare_obj=w;
+//		this.LinkedWare_string=w.getname();
+		this.MaxItems=max;
 	}
+	
+//	String getware()
+//	{
+//		return this.LinkedWare_string;
+//	}
 	
 	int getmaxitems()
 	{
