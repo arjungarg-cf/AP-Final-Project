f683314c084682dda7a9c0a595744037f5333ccb
shankarprakriya <shankarprakriya@192.168.51.67>
2018-12-02 16:33:57 +0530

    Push 2 of Java Doc

--- APproject/src/data/Category.java
+++ APproject/src/data/Category.java
@@ -1,11 +1,24 @@
 package data;
-//Complete mostlikely
+
 import java.util.ArrayList;
 import java.util.Comparator;
 
+
+
+/**
+ * This class contains an ArrayList of sub-category type objects
+ * @author shankarprakriya
+ *
+ */
 public class Category implements Comparable<Category> {
 	
+	/**
+	 * This is an ArrayList of sub category type objects that are under category
+	 */
 	ArrayList<Sub_Category> subcat;
+	/**
+	 * String value holding the name of the category
+	 */
 	private String name;
 	//CONSTRUCTOR
 	Category(String n){
@@ -13,6 +26,9 @@ public class Category implements Comparable<Category> {
 		this.subcat=null;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
 	public int compareTo(Category o)
 	{
@@ -24,17 +40,26 @@ public class Category implements Comparable<Category> {
 			return -1;
 	}
 	
-	//GETTER AND SETTER
+	
+	/**
+	 * @return returns String  type name of the Category
+	 */
 	String getname()
 	{
 		return this.name;
 	}
+	/**
+	 * @param s is a String type object used to set name of the Category
+	 */
 	void setname(String s)
 	{
 		this.name=s;
 	}
 	
-	//other
+	
+	/**
+	 * @param s is a Sub_Category type object passed to add sub category to this category 
+	 */
 	void addsubcat(Sub_Category s) {
 		this.subcat.add(s);
 	}
--- APproject/src/data/Store.java
+++ APproject/src/data/Store.java
@@ -2,16 +2,44 @@ package data;
 
 import java.util.ArrayList;
 
+
+
+/**
+ * @author shankarprakriya
+ * 
+ */
 public class Store extends Superstore {
+	/**
+	 * Admin is a StoreAdministrator type object which represents teh Store Admin of the store
+	 */
 	private StoreAdministrator Admin;
-//	private String LinkedWare_string;
+
+	/**
+	 * LinkedWare_obj is Warehouse type object representing the Warehouse that the STore is linked to
+	 */
 	private Warehouse LinkedWare_obj;
+	/**
+	 *  MaxItems is and integer representing the maximum number of items a store can hold 
+	 */
 	private final int MaxItems;
+	/**
+	 *  String type ID and Name of the Store
+	 */
 	private String ID,Name;
+	/**
+	 * Storess is a String type Arraylist holding Names of all the Stores made 
+	 */
 	static ArrayList<String> Storess;
+	/**
+	 *  Stores is a Store type ArrayList holding store type references to all Stores made
+	 */
 	static ArrayList<Store> Stores;
+	/**
+	 * Categories is an ArrayList of Category type objects which contains the entire database of Items that the store has
+	 */
 	ArrayList<Category> Categories;
 	
+	//Constructor
 	Store(String id,String name,Warehouse w,int max){
 		this.ID=id;
 		this.Name=name;
@@ -45,30 +73,41 @@ public class Store extends Superstore {
 		
 	}
 	
+
 	
-//	String getware()
-//	{	
-//		return this.LinkedWare_string;
-//	}
-	
+	/**
+	 * @return Integer type number denoting the maximum number of Items a Store can contain
+	 */
 	int getmaxitems()
 	{
 		return this.MaxItems;
 	}
 	
+	/**
+	 * @return String type value denoting the Name of the Store
+	 */
 	public String getname()
 	{
 		return this.Name;
 	}
+	/**
+	 * @return String type value denoting the ID of the Store
+	 */
 	public String getID()
 	{
 		return this.ID;
 	}
+	/**
+	 * @param w denotes a Warehouse type object passed to set the Warehouse linked to the Store
+	 */
 	void setWarehouse(Warehouse w)
 	{
 		this.LinkedWare_obj=w;
 	}
 	
+	/**
+	 * @param s is a Store Administrator type object passed to set the Store Administrator linked to the Store
+	 */
 	void setAdmin(StoreAdministrator s) {
 		this.Admin=s;
 	}
